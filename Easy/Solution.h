2024830e03a791fad065093880cbd9d8253eb389
Kremenevskiy <vkremenevskiy@gmail.com>
2020-10-02 15:45:36 +0300

    little changes

---
+++ Easy/Solution.h
@@ -0,0 +1,13 @@
+#ifndef EASY_SOLUTION_H
+#define EASY_SOLUTION_H
+
+#include <vector>
+#include <unordered_map>
+
+class Solution{
+public:
+    std::vector<int> twoSum(const std::vector<int>& nums, int target);
+    int searchInsert(std::vector<int> &nums, int target);
+};
+
+#endif //EASY_SOLUTION_H
